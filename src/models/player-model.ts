5e360361ab468df2af42c8c43a932ce7eaed677d
Polar0471 <rodrigoarth512@gmail.com>
2024-07-09 17:01:41 -0300

    interface PlayerStatistics {

--- src/models/player-model.ts
+++ src/models/player-model.ts
@@ -1,4 +1,18 @@
+interface PlayerStatistics {
+    Overall: number;
+    Pace: number;
+    Shooting: number;
+    Passing: number;
+    Dribbling: number;
+    Defending: number;
+    Physical: number;
+}
+
 export interface PlayerModel {
     id: number;
     name: string;
+    club: string;
+    nationality: string;
+    position: string;
+    statistics: PlayerStatistics;
 }
--- src/repositories/players-repository.ts
+++ src/repositories/players-repository.ts
@@ -1,8 +1,86 @@
 import { PlayerModel } from "../models/player-model"
 
 const database: PlayerModel[] = [
-    {id: 1, name: "Messi"},
-    {id: 2, name: "Ronaldo"}
+    {
+        id: 1,
+        name: "Lionel Messi",
+        club: "Paris Saint-Germain",
+        nationality: "Argentina",
+        position: "Forward",
+        statistics: {
+            Overall: 93,
+            Pace: 85, 
+            Shooting: 94,
+            Passing: 91,
+            Dribbling: 95,
+            Defending: 38,
+            Physical: 65
+        }      
+    },
+    {
+        id: 2,
+        name: "Cristiano Ronaldo",
+        club: "Manchester United",
+        nationality: "Portugal",
+        position: "Forward",
+        statistics: {
+            Overall: 92,
+            Pace: 87, 
+            Shooting: 93,
+            Passing: 82,
+            Dribbling: 89,
+            Defending: 35,
+            Physical: 77
+        }      
+    },
+    {
+        id: 3,
+        name: "Kevin De Bruyne",
+        club: "Manchester City",
+        nationality: "Belgium",
+        position: "Midfielder",
+        statistics: {
+            Overall: 91,
+            Pace: 76, 
+            Shooting: 86,
+            Passing: 93,
+            Dribbling: 88,
+            Defending: 64,
+            Physical: 78
+        }      
+    },
+    {
+        id: 4,
+        name: "Robert Lewandowski",
+        club: "Barcelona",
+        nationality: "Poland",
+        position: "Forward",
+        statistics: {
+            Overall: 92,
+            Pace: 78, 
+            Shooting: 94,
+            Passing: 79,
+            Dribbling: 86,
+            Defending: 43,
+            Physical: 82
+        }      
+    },
+    {
+        id: 5,
+        name: "Kylian Mbappé",
+        club: "Paris Saint-Germain",
+        nationality: "France",
+        position: "Forward",
+        statistics: {
+            Overall: 91,
+            Pace: 97, 
+            Shooting: 89,
+            Passing: 80,
+            Dribbling: 92,
+            Defending: 36,
+            Physical: 76
+        }      
+    }
 ]
 
 export const findAllPlayers = async (): Promise<PlayerModel[]> => {
