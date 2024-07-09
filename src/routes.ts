d49e16e23c69780a1571e3b0fb350bdc9295caf6
Polar0471 <rodrigoarth512@gmail.com>
2024-07-09 20:29:20 -0300

    Adicionando recursos de clubs

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import * as PlayerController from "./controllers/players-controller";
+import * as ClubController from "./controllers/clubs-controller"
 
 // a router é quem deverá apontar para um controller
 
@@ -7,6 +8,7 @@ const router = Router() // gerenciador de rotas
 
 router.get("/players", PlayerController.getPlayer)
 router.get("/players/:id", PlayerController.getPlayerById) // router param 
+router.get("/clubs", ClubController.getClubs)
 
 router.post("/players", PlayerController.postPlayer)
 
